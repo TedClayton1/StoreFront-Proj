d1aede753477c0eb9d75d398f941b58e783088d8
TedClayton1 <ted_clayton@hotmail.com>
2022-01-06 00:51:28 -0500

    I added two new apps which was store and tags and added them both to installed apps in the settings.py file.

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
+    'debug_toolbar',
+    'store',
+    'tags',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,7 +51,18 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type("image/png", ".png", True)
 
 ROOT_URLCONF = 'storefront.urls'
 
